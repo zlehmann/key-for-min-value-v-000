0e2a1b6d11ff49e4db4703c54529b67a54c9dfc3
Zachary Lehmann <zlehmann@gmail.com>
2018-09-20 01:34:19 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   lowest_value = 0
-  lowest_key = "empty"
+  lowest_key = "key name here"
   name_hash.collect do |key, value|
     if value < lowest_value
       lowest_value = value
-      lowest_key = key
+      lowest_key = name_hash[key]
     end
   end
   return lowest_key
